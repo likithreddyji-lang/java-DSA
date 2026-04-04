6bc66c746bf295410046475859552ada00d7419c
likith <likithreddyji@gmail.com>
2026-04-04 20:14:50 +0530

    fourth commit

---
+++ src/sorting.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class sorting {
+    //same mundu problem ee but only using array concept sorting
+    public static void main(String[]args){
+        int arr[]={1,2,2,3,4,5,2};
+        int n=arr.length/2;
+        Arrays.sort(arr);//array pettakudadu arrays pettali only array pedithe error vastadi
+        System.out.println(arr[n]);
+
+
+    }
+}
